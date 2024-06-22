92bb925c4f0c1a0d29b1b1dd603476a85d28233e
liuqq <liuqq@giec.cn>
2024-06-22 14:08:12 +0800

    add service of bluetooth

--- recipes-devtools/android-tools/files/adbd.sh
+++ recipes-devtools/android-tools/files/adbd.sh
@@ -54,5 +54,12 @@ case "$1" in
 		echo "Usage: $0 {start|stop|restart}"
 		exit 1
 esac
-
+echo 0 > /sys/class/rfkill/rfkill0/state
+echo 0 > /proc/bluetooth/sleep/btwrite
+sleep 0.5
+echo 1 > /sys/class/rfkill/rfkill0/state
+echo 1 > /proc/bluetooth/sleep/btwrite
+sleep 0.5
+insmod  /lib/modules/5.10.198-rockchip-standard/kernel/drivers/bluetooth/bluetooth_uart_driver/hci_uart.ko
+rtk_hciattach -n -s 115200 /dev/ttyS8 rtk_h5 &
 exit 0
